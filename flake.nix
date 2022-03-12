476b09d7b8aaa7fe2b01e72760f5b55d6ec83894
hugosenari <hugosenari@gmail.com>
2022-03-12 02:40:21 -0300

    chore: re index rexpaint

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''REXPaint .xp parser'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."rexpaint-master".url = "path:./master";
+  inputs."rexpaint-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."rexpaint-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''REXPaint .xp parser'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-rexpaint-master.flake = false;
+  inputs.src-rexpaint-master.owner = "irskep";
+  inputs.src-rexpaint-master.ref   = "refs/heads/master";
+  inputs.src-rexpaint-master.repo  = "rexpaint_nim";
+  inputs.src-rexpaint-master.type  = "github";
+  
+  inputs."zip".dir   = "nimpkgs/z/zip";
+  inputs."zip".owner = "riinr";
+  inputs."zip".ref   = "flake-pinning";
+  inputs."zip".repo  = "flake-nimble";
+  inputs."zip".type  = "github";
+  inputs."zip".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zip".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-rexpaint-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-rexpaint-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
